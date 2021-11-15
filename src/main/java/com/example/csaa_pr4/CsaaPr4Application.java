5b072c63eb4bf59c9a155708f4fb346654f3269e
sorokow <sorokov_a02@mail.ru>
2021-11-16 00:21:23 +0300

    Initial commit

---
+++ src/main/java/com/example/csaa_pr4/CsaaPr4Application.java
@@ -0,0 +1,13 @@
+package com.example.csaa_pr4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CsaaPr4Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CsaaPr4Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/csaa_pr4/CsaaPr4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.csaa_pr4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CsaaPr4ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
